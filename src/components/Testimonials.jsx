1432721e3002aef7fc5e0eb5cc06d18ce0098f5f
Manavvv07 <manavsingh29sk@gmail.com>
2026-01-28 17:23:03 +0530

    minor changes

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
         <div className="flex flex-wrap justify-center">
             {testimonials.map((testimonial, index) => (
                 <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
-                    <div className="bg-neutral rounded-md p-6 text-md border border-neutral-700 font-thin h-full flex flex-col justify-between cursor-pointer transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-xl hover:bg-black">
+                    <div className="bg-neutral rounded-md p-6 text-md border border-neutral-700 font-extralight h-full flex flex-col justify-between cursor-pointer transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-xl hover:bg-black">
                         <p>{testimonial.text}</p>
                         <div className="flex mt-8 items-start">
                             <img className="w-12 h-12 mr-6 rounded-full border border-neutral-300" src={testimonial.image} alt={testimonial.user} />
